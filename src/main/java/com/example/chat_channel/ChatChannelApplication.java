0a19f6d38f04fc1f48cbeeb1df952a73c510ae5c
“madeleineMU24” <3tomu24.thunma@folkuniversitetet.nu>
2025-03-12 13:43:24 +0100

    first commit

---
+++ src/main/java/com/example/chat_channel/ChatChannelApplication.java
@@ -0,0 +1,13 @@
+package com.example.chat_channel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatChannelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatChannelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chat_channel/ChatChannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_channel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatChannelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
